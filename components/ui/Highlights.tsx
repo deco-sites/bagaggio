55ebad3b782c3973439c68051cfc24d4794ed186
Erick Nascimento <ericksouzadonascimento@gmail.com>
2023-04-19 16:37:24 -0300

    feat: highlights section done

--- components/ui/Highlights.tsx
+++ components/ui/Highlights.tsx
@@ -1,8 +1,12 @@
 import Image from "deco-sites/std/components/Image.tsx";
+import Icon from "deco-sites/fashion/components/ui/Icon.tsx";
+import Button from "deco-sites/fashion/components/ui/Button.tsx";
 import Container from "deco-sites/fashion/components/ui/Container.tsx";
 import Text from "deco-sites/fashion/components/ui/Text.tsx";
 import { Slider } from "deco-sites/fashion/components/ui/Slider.tsx";
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import SliderControllerJS from "deco-sites/fashion/islands/SliderJS.tsx";
+import { useId } from "preact/hooks";
 
 export interface Highlight {
   src: LiveImage;
@@ -16,33 +20,79 @@ export interface Props {
   title: string;
 }
 
-function Highlights({ highlights = [], title }: Props) {
+function Controls() {
   return (
-    <Container class="grid grid-cols-1 grid-rows-[48px_1fr] py-10">
-      <h2 class="text-center">
-        <Text variant="heading-2">{title}</Text>
-      </h2>
+    <>
+      <div class="flex items-center justify-center z-10 col-start-3 row-start-2 absolute top-[35%] left-3 sm:-left-6 bg-white sm:bg-[#FFFFFF95] rounded-full shadow-xl">
+        <Button
+          class="h-8 w-8 sm:h-14 sm:w-14"
+          variant="icon"
+          data-slide="prev"
+          aria-label="Previous item"
+        >
+          <Icon
+            class=""
+            size={32}
+            id="ChevronLeft"
+            strokeWidth={1.5}
+          />
+        </Button>
+      </div>
+      <div class="flex items-center justify-center z-10 col-start-3 row-start-2 absolute top-[35%] right-3 sm:-right-6 bg-white sm:bg-[#FFFFFF95] rounded-full shadow-xl">
+        <Button
+          class="h-8 w-8 sm:h-14 sm:w-14"
+          variant="icon"
+          data-slide="next"
+          aria-label="Next item"
+        >
+          <Icon
+            class=""
+            size={32}
+            id="ChevronRight"
+            strokeWidth={1.5}
+          />
+        </Button>
+      </div>
+    </>
+  );
+}
 
-      <Slider
-        class="gap-6"
-        snap="snap-center sm:snap-start block first:ml-6 sm:first:ml-0 last:mr-6 sm:last:mr-0"
-      >
-        {highlights.map(({ href, src, alt, label }) => (
-          <a
-            href={href}
-            class="flex flex-col gap-4 items-center min-w-[190px]"
-          >
-            <Image
-              class="rounded-[40px]"
-              src={src}
-              alt={alt}
-              width={190}
-              height={265}
-            />
-            <Text variant="body">{label}</Text>
-          </a>
-        ))}
-      </Slider>
+function Highlights({ highlights = [], title }: Props) {
+  const id = useId();
+  return (
+    <Container class="grid grid-cols-1 grid-rows-[138px_1fr]">
+      <h2 class="text-center mt-[64px]">
+        <Text
+          variant="heading-2"
+          class="text-[32px] leading-[38px] !font-semibold"
+        >
+          {title}
+        </Text>
+      </h2>
+      <div id={id} class="relative p-4">
+        <Slider
+          class=" sm:gap-16 scrollbar-none"
+          snap="snap-center sm:snap-start block first:ml-6 sm:first:ml-0 last:mr-6 sm:last:mr-0"
+        >
+          {highlights.map(({ href, src, alt, label }) => (
+            <a
+              href={href}
+              class="flex flex-col gap-4 items-center min-w-[232px]"
+            >
+              <Image
+                class=""
+                src={src}
+                alt={alt}
+                width={232}
+                height={248}
+              />
+              <Text variant="body" class="!font-bold text-xl">{label}</Text>
+            </a>
+          ))}
+        </Slider>
+        <Controls />
+        <SliderControllerJS rootId={id} />
+      </div>
     </Container>
   );
 }
